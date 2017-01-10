88045fb7a1b1ccbed6e41f828325fe1af4bc8b21
Grant Zukowski <grantzukowski@gmail.com>
2017-01-10 10:10:44 -0800

    JavaDoc comments added
    Feel free to change/modify

--- APCSArrayList.java
+++ APCSArrayList.java
@@ -3,14 +3,59 @@
  * Object oriented assignment for CS 1.
  * 
  * @author zukowskig, Dmitriy Cheryak
+ * @version 0.1
  */
 public interface APCSArrayList {
-	public void add(int item); // Add item to the end
-	public void add(int index, int item); // Add item to the specified index, move everything else up
-	public int get(int index); // Gets item at specified index
-	public int size(); // Returns number of items
-	public int set(int index, int item); // Replaces item at index with specified item and returns old value)
+	/**
+	 * Add item to the end
+	 * @param item
+	 */
+	public void add(int item);
+	
+	/**
+	 * Add item to the specified index, move everything else up
+	 * @param index
+	 * @param item
+	 */
+	public void add(int index, int item);
+	
+	/**
+	 * Gets item at specified index
+	 * @param index
+	 * @return
+	 */
+	public int get(int index);
+	
+	/**
+	 * Returns number of items
+	 * @return
+	 */
+	public int size();
+	
+	/**
+	 * Replaces item at index with specified item and returns old value
+	 * @param index
+	 * @param item
+	 * @return
+	 */
+	public int set(int index, int item);
+	
+	/**
+	 * Searches through array to find item
+	 * @param item
+	 * @return
+	 */
 	public boolean contains(int item);
+	
+	/**
+	 * Removes item at specified index
+	 * @param index
+	 * @return
+	 */
 	public int remove(int index);
+	
+	/**
+	 * Private method to resize the array under certain circumstances
+	 */
 	void resize();
 }
