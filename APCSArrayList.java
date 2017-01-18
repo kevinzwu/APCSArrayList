cbb6298c2ff829145a69bfab9c141adbe6b846cf
Dmitriy Chernyak <Dmitriy Chernyak>
2017-01-18 06:44:23 -0800

    Add writeup and update interface to match

--- APCSArrayList.java
+++ APCSArrayList.java
@@ -5,54 +5,53 @@
  * @author zukowskig, Dmitriy Cheryak
  * @version 0.1
  */
-public interface APCSArrayList {
+public interface APIntList {
+
 	/**
-	 * Add item to the end
+	 * Returns the size of the list
+	 */
+	int size();
+	
+	/**
+	 * Adds the item to the end of the list
 	 * @param item
 	 */
-	public void add(int item);
-	
+        void add(int item);
+
 	/**
 	 * Add item to the specified index, move everything else up
 	 * @param index
 	 * @param item
 	 */
-	public void add(int index, int item);
+	void add(int index, int item);
+
+	/**
+	 * Replaces element at index with item
+	 * @param index
+	 * @param item
+	 */
+	void set(int index, int item);
 	
 	/**
 	 * Gets item at specified index
 	 * @param index
 	 * @return
 	 */
-	public int get(int index);
+	int get(int index);
 	
 	/**
-	 * Returns number of items
-	 * @return
-	 */
-	public int size();
-	
-	/**
-	 * Replaces item at index with specified item and returns old value
-	 * @param index
+	 * Uncomment me if you're doing extra credit
+	 * and replace with appropriate documentation
 	 * @param item
 	 * @return
 	 */
-	public int set(int index, int item);
+	// boolean contains(int item);
 	
 	/**
-	 * Searches through array to find item
-	 * @param item
-	 * @return
-	 */
-	public boolean contains(int item);
-	
-	/**
-	 * Removes item at specified index
+	 * Uncomment me if you're doing extra credit
+	 * and replace with appropriate documentation
 	 * @param index
 	 * @return
 	 */
-	public int remove(int index);
-	
-
+	// int remove(int index);
 }
